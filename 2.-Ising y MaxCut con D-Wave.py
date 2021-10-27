5c0270f649451fbd702970fb5eb0752930819897
EliasCombarro <47427406+EliasCombarro@users.noreply.github.com>
2021-10-27 10:58:40 +0200

    Add files via upload

---
+++ 2.-Ising y MaxCut con D-Wave.py
@@ -0,0 +1,92 @@
+import numpy as np
+import dimod
+
+# Especificar los coeficientes del problema que queremos resolver es muy fácil
+# Empezaremos con un caso muy sencillo
+
+J = {(0,1):1}
+
+h = {}
+model = dimod.BinaryQuadraticModel(h, J, 0.0, dimod.SPIN)
+
+print("El modelo que vamos a resolver es")
+print(model)
+print()
+
+# Podemos resolver el modelo de forma exacta
+
+
+from dimod.reference.samplers import ExactSolver
+sampler = ExactSolver()
+solution = sampler.sample(model)
+print("La solucion exacta es")
+print(solution)
+print()
+
+
+# O con *simulated annealing* (un método heurístico de optimización para ordenadores clásicos)
+
+
+sampler = dimod.SimulatedAnnealingSampler()
+response = sampler.sample(model, num_reads=10)
+print("La solucion con simulated annealing es")
+print(response)
+print()
+
+
+# Y, por supuesto, con el ordenador cuántico de D-Wave 
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+sampler = EmbeddingComposite(DWaveSampler())
+sampler_name = sampler.properties['child_properties']['chip_id']
+print("Estamos usando el ordenador de D-Wave llamado", sampler_name)
+response = sampler.sample(model, num_reads=100)
+print("La solucion con el quantum annealer de D-Wave es")
+print(response)
+print()
+
+print()
+print()
+print()
+
+
+# Veamos ahora un caso un poco más complicado
+
+J = {(0,1):1,(0,2):1,(1,2):1,(1,3):1,(2,4):1,(3,4):1}
+h = {}
+model = dimod.BinaryQuadraticModel(h, J, 0.0, dimod.SPIN)
+print("El modelo que vamos a resolver es")
+print(model)
+print()
+
+
+# Primero lo resolvemos de forma exacta
+
+sampler = ExactSolver()
+solution = sampler.sample(model)
+print("La solucion exacta es")
+print(solution)
+print()
+
+
+# Ahora, con *simulated annealing*
+
+sampler = dimod.SimulatedAnnealingSampler()
+response = sampler.sample(model, num_reads=10)
+print("La solucion con simulated annealing es")
+print(response)
+print()
+
+
+# Finalmente, lo resolvemos nuevamente con el *quantum annealer*
+
+
+sampler = EmbeddingComposite(DWaveSampler(solver='Advantage_system1.1'))
+sampler_name = sampler.properties['child_properties']['chip_id']
+print("Estamos usando el ordenador de D-Wave llamado", sampler_name)
+response = sampler.sample(model, num_reads=100)
+print("La solucion con el quantum annealer de D-Wave es")
+print(response)
+print()
+
---
+++ 3.-De problemas con restricciones a QUBO.py
@@ -0,0 +1,98 @@
+# En este ejemplo vamos a ver cómo definir un problema con restricciones, 
+# transformalo en una instancia QUBO y resolverlo con distintos métodos
+
+from dimod import *
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+
+# Creamos un Constrained Quadratic Model
+
+qp = CQM()
+
+# Creamos las variables 
+
+x = Binary("x")
+y = Binary("y")
+z = Binary("z")
+
+# Añadimos una restricción lineal
+
+qp.add_constraint(x + 2*y + 3*z <= 5)
+
+# Añadimos la función objetivo
+
+qp.set_objective(2*x*y -y - 4*z*y)
+
+# El problema que hemos definido tiene las siguientes propiedades
+
+print("El modelo inicial es")
+print("")
+
+print("Variables", qp.variables)
+print("Objetivo", qp.objective)
+print("Restricciones", qp.constraints)
+print("")
+
+# Lo convertimos a QUBO
+
+M = 100 # Constante de penalización para las restricciones
+
+qubo, invert = dimod.cqm_to_bqm(qp, lagrange_multiplier = M)
+
+# El nuevo problema tiene las siguientes propiedades
+
+print("El modelo convertido es")
+print("")
+
+print("Variables", qubo.variables)
+print("Objetivo, parte lineal", qubo.linear)
+print("Objetivo, parte cuadrática", qubo.quadratic)
+print("")
+
+# Podemos resolver como hacíamos antes
+
+# De forma exacta
+
+sampler = ExactSolver()
+solution = sampler.sample(qubo)
+print("La solucion exacta es")
+print(solution)
+print()
+
+# Podemos convertir las soluciones del problema qubo en soluciones del problema original
+
+# La mejor es
+
+print("La mejor solución es", invert(solution.first.sample))
+print("")
+
+# Podemos enumerarlas todas
+
+for s in solution.data():
+    print(invert(s.sample),qp.objective.energy(s.sample),qp.check_feasible(s.sample))
+
+print("")
+    
+# Con simulated annealing
+
+sampler = dimod.SimulatedAnnealingSampler()
+solution  = sampler.sample(qubo, num_reads=10)
+print("La solucion con simulated annealing es")
+print("")
+
+for s in solution.data():
+    print(invert(s.sample),qp.objective.energy(s.sample),qp.check_feasible(s.sample))
+    
+print("")
+
+# Con el ordenador cuántico 
+
+sampler = EmbeddingComposite(DWaveSampler())
+solution = sampler.sample(qubo, num_reads=100)
+
+print("La solucion con el quantum annealer es")
+print("")
+
+for s in solution.data():
+    print(invert(s.sample),qp.objective.energy(s.sample),qp.check_feasible(s.sample), s.num_occurrences)
+
